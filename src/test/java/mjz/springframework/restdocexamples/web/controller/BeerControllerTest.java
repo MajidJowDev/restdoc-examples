f6fcd0df9bdc000903b96cdbeb4028a18502dbab
MajidJowDev <majid.jow@gmail.com>
2023-02-04 00:38:52 +0330

    28- 8. Documenting Responses... added response fields (POJO object fields)

--- src/test/java/mjz/springframework/restdocexamples/web/controller/BeerControllerTest.java
+++ src/test/java/mjz/springframework/restdocexamples/web/controller/BeerControllerTest.java
@@ -28,6 +28,8 @@ import static org.mockito.BDDMockito.given;
 //import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static  org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*; // DO NOT FORGET to use this import if you want to leverage RestDoc
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.restdocs.request.RequestDocumentation.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -64,7 +66,18 @@ class BeerControllerTest {
                         ),
                         requestParameters(
                                 parameterWithName("iscold").description("Is Beer Cold Query param")
-                                )));
+                                ),
+                        responseFields( // if we miss a field that we added to the response in controller response POJO, we get an error
+                                fieldWithPath("id").description("Id of Beer"),
+                                fieldWithPath("version").description("Version number"),
+                                fieldWithPath("createdDate").description("Date Created"),
+                                fieldWithPath("lastModifiedDate").description("Date Updated"),
+                                fieldWithPath("beerName").description("Beer Name"),
+                                fieldWithPath("beerStyle").description("Beer Style"),
+                                fieldWithPath("upc").description("UPC of Beer"),
+                                fieldWithPath("price").description("Price"),
+                                fieldWithPath("quantityOnHand").description("Quantity On hand")
+                        )));
 
 
         /*
