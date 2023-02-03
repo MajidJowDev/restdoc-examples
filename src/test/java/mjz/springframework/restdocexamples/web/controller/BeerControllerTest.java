81a471b13e1579a8cb972a3e3717f995b62305fa
MajidJowDev <majid.jow@gmail.com>
2023-02-04 00:25:49 +0330

    28- 7. Documenting Query Parameters... added Request Parameters doc to test

--- src/test/java/mjz/springframework/restdocexamples/web/controller/BeerControllerTest.java
+++ src/test/java/mjz/springframework/restdocexamples/web/controller/BeerControllerTest.java
@@ -28,8 +28,7 @@ import static org.mockito.BDDMockito.given;
 //import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static  org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*; // DO NOT FORGET to use this import if you want to leverage RestDoc
-import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
-import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.restdocs.request.RequestDocumentation.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 //for setting up mockMvc with the RestDoc we need to add two following annotations
@@ -55,11 +54,17 @@ class BeerControllerTest {
 
         // for restdocs we need to change the get() import from MockMvcRequestBuilders to RestDocumentationRequestBuilders
         // the {beerId} is going to tell RestDoc about the path parameter
-        mockMvc.perform(get("/api/v1/beer/{beerId}", UUID.randomUUID().toString()).accept(MediaType.APPLICATION_JSON))
+        mockMvc.perform(get("/api/v1/beer/{beerId}", UUID.randomUUID().toString())
+                        .param("iscold", "yes") // this controller does not actually accept query parameters and is going to ignore this param, but we added it as an example
+                        .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andDo(document("v1/beer", pathParameters( // setting endpoint address and parameters
-                        parameterWithName("beerId").description("UUID of desired beer to get.")
-                )));
+                .andDo(document("v1/beer",
+                        pathParameters( // setting endpoint address and parameters
+                            parameterWithName("beerId").description("UUID of desired beer to get.")
+                        ),
+                        requestParameters(
+                                parameterWithName("iscold").description("Is Beer Cold Query param")
+                                )));
 
 
         /*
